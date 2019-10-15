bcf6bf5f03569f5a5c426dd3bc5dd0de645c7650
Dheeraj M Pai <37558464+dheerajpai@users.noreply.github.com>
2019-10-15 19:41:04 +0530

    Add files via upload

---
+++ reactapp/precache-manifest.17bbc3a0979bd3cc81e532dde2cf88c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f89c99fb8465edd81c9d4f26985d16c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "875786ad919270d26331",
+    "url": "/static/css/main.11306bcb.chunk.css"
+  },
+  {
+    "revision": "df4a0063216bb07f008e",
+    "url": "/static/js/2.04405229.chunk.js"
+  },
+  {
+    "revision": "875786ad919270d26331",
+    "url": "/static/js/main.60f876d6.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
